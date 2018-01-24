becddbe61376b043900be6ec36d0d7c0f1032c1a
stillNovice <deepaksinghtomar7@gmail.com>
2018-01-24 21:04:36 +0530

    functionality done

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
-//var showHideStuff;
+var isSearchTextChanged = false;
+var $searchText, $searchBtn, $searchResultDiv, $randomArticleDiv, $h1, $searchIconAtStart, $actualSearch;
 
 $(document).ready(function() {
+  $searchText = $('.searchText');
+  $searchBtn = $('.searchBtn');
+  $searchResultDiv = $('#searchResultDiv');
+  $randomArticleDiv = $('.randomArticleDiv');
+  $h1 = $('h1');
+  $searchIconAtStart = $('.searchIconAtStart');
+  $actualSearch = $('.actualSearch');
 
-  $('.searchBtn').on('click', function() {
-    var searchText = $('.searchText').val().trim();
+  $searchText.keyup(function(event) {
+    if(event.keyCode == 13) {
+      $searchBtn.click();
+    }
+
+    event.preventDefault();
+  });
+
+  $searchText.on('change', function(event) {
+    isSearchTextChanged = true;
+  });
+
+  $searchBtn.on('click', function(event) {
+    if(isSearchTextChanged == false) {
+      return;
+    }
+
+    isSearchTextChanged = false;
+
+    var searchText = $searchText.val().trim();
 
     if(searchText == "") {
-      $('.searchText').val('');
+      $searchText.val('');
       return;
     }
 
     // Remove other components from DOM
-    $('h1').slideUp('slow');
-    $('.randomArticleDiv').fadeOut('slow');
+    $h1.slideUp('slow');
+    $randomArticleDiv.fadeOut('slow');
 
     showSearchResult(searchText);
+
+    event.preventDefault();
   });
 
 });
@@ -27,12 +55,14 @@ function showSearchResult(searchText) {
       action: 'query',
       list: 'search',
       srsearch: searchText,
-      format: 'json',
-      cllimit: '20'
+      format: 'json'
     },
     dataType: 'jsonp',
     success: function(data) {
       populateCards(data);
+    },
+    failure: function(err) {
+      console.error(err);
     }
   })
 }
@@ -42,20 +72,41 @@ function populateCards(data) {
   var resultString = "";
   // when search result is empty
   if(!(data.query.search && data.query.search.length)) {
-    resultString += '<p class="text-center" style="font-weight: bold; font-size: 48px;">Nothing to show here!!!</p>';
+    resultString += '<p class="text-center" style="font-weight: bold; font-size: 48px;">Nothing To Show Here!!!</p>';
   } else {
+    for (var i = 0; i < data.query.search.length; i++) {
+      var obj = data.query.search[i];
 
+      resultString += '<div class="row justify-content-md-center">';
+      resultString += '<div class="col-8">';
+      resultString += '<div class="card">';
+      resultString += '<div class="row">';
+      resultString += '<div class="col-4">';
+
+      resultString += ' <img class="img-fluid logoOnCard" src="http://www.widewallpapershd.info/file/1338/728x544/16:9/puzzle-silver-globe_1660297167.jpg" alt="wikipedia logo">';
+      resultString += '</div>';
+
+      resultString += '<div class="col-8">';
+      resultString += '<div class="card-body">';
+
+      resultString += '<a href="https://en.wikipedia.org/wiki/' + obj.title.toString().split(' ').join('_') + '" target="_blank" style="text-decoration: none;">';
+      resultString += '<h5 class="card-title">' + obj.title + '</h5></a>';
+
+      resultString += '<p>' + obj.snippet + '</p>';
+      resultString += '</div></div></div></div></div></div><br/>';
+    }
   }
 
   console.log('resultString', resultString);
-  
-  $('#searchResultDiv').html('');
-  $('#searchResultDiv').append(resultString);
+
+  $randomArticleDiv.remove();
+  $searchResultDiv.html('');
+  $searchResultDiv.append(resultString);
 }
 
 function showHideStuff() {
-  $('.searchIconAtStart').fadeOut('', 'swing', function () {
-    $('.actualSearch').fadeIn('slow', 'swing');
+  $searchIconAtStart.fadeOut('', 'swing', function () {
+    $actualSearch.fadeIn('slow', 'swing');
   });
-};
+}
 
